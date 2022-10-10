a4cc139d84634e7325739553a23dc44c88b972d8
Ethem <koraygokdeniz@gmail.com>
2022-10-10 16:03:15 -0400

    updated Ts7_86_Automation.java created Ts7_86_Automation_TC2.java

--- src/test/java/com/vytrack/tests/Ts7_86_Automation_TC2.java
+++ src/test/java/com/vytrack/tests/Ts7_86_Automation_TC2.java
@@ -1,19 +1,16 @@
 package com.vytrack.tests;
 
-import com.google.common.base.Verify;
 import com.vytrack.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class Ts7_86_Automation {
+public class Ts7_86_Automation_TC2 {
     WebDriver driver;
 
     @BeforeMethod
@@ -23,13 +20,11 @@ public class Ts7_86_Automation {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://qa2.vytrack.com/user/login");
-
-
     }
 
     @Test
 
-    public void UsersTc1() {
+    public void UsersTc2() {
 
         //Users login with valid credentials
 
@@ -46,21 +41,14 @@ public class Ts7_86_Automation {
         WebElement howToUseBar = driver.findElement(By.xpath("//*[@id=\"pinbar\"]/div[1]/div/a"));
         howToUseBar.click();
 
-        //Verify the users see 2 messages: “How To Use Pinbar”
+        //Verify Image Source
 
-        WebElement howToUseBarText = driver.findElement(By.xpath("//h3[.='How To Use Pinbar']"));
-        String actualText = howToUseBarText.getText();
-        String expectedText = "How To Use Pinbar";
-        Assert.assertEquals(actualText, expectedText);
-
-        // Verify: “Use the pin icon on the right top corner of the page to create fast access link in the pinbar.”
-
-        WebElement pinIconText = driver.findElement(By.xpath("//*[@id=\"container\"]/div[2]/div/p[1]"));
-        String actualText2 = pinIconText.getText();
-        String expectedText2 = "Use the pin icon on the right top corner of the page to create fast access link in the pinbar.";
-        Assert.assertEquals(actualText2, expectedText2);
+        WebElement imgSrc = driver.findElement(By.xpath("//img[@src='/bundles/oronavigation/images/pinbar-location.jpg']"));
+        String actualSrc = imgSrc.getAttribute("src");
+        System.out.println("Image source: " + actualSrc + " -verified!");
 
     }
+
 
     @AfterMethod
 
@@ -70,7 +58,3 @@ public class Ts7_86_Automation {
 
 
 }
-
-
-
-
