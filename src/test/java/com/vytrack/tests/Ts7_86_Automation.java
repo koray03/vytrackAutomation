a5312e5ccb0a9a3aafe10db04632ac79c1720b35
Ethem <koraygokdeniz@gmail.com>
2022-10-10 14:26:51 -0400

    merged rollaT code into ethem

--- src/test/java/com/vytrack/tests/Ts7_86_Automation.java
+++ src/test/java/com/vytrack/tests/Ts7_86_Automation.java
@@ -12,14 +12,6 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class Ts7_86_Automation {
-    //Write your code here!
-    // Rolla joined
-
-    // ferhat was here
-
-    //EthemG is Ready
-
-
    WebDriver driver;
 
    @BeforeMethod
