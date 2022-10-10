188ee2496c5733e28d56f92f06b70cc9a5f7653b
Ecaterina <ecaterinacoseru@gmail.com>
2022-10-10 14:30:07 -0400

    class updated

--- src/test/java/com/vytrack/tests/Ts7_86_Automation.java
+++ src/test/java/com/vytrack/tests/Ts7_86_Automation.java
@@ -1,7 +1,12 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class Ts7_86_Automation {
     //Write your code here!
