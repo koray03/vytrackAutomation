b57132ca64c3570e3a572986beac2e37090b444f
Ethem <koraygokdeniz@gmail.com>
2022-10-10 11:11:19 -0400

    Created Repository for Ts7_86_Automation.java

---
+++ src/test/java/com/vytrack/tests/Ts7_86_Automation.java
@@ -0,0 +1,13 @@
+package com.vytrack.tests;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Ts7_86_Automation {
+    //Write your code here!
+
+
+
+
+
+}
