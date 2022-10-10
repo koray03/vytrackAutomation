7a09a0f80fe4bdbc14deaf25594e313c6224f1aa
Ethem <koraygokdeniz@gmail.com>
2022-10-10 15:06:51 -0400

    updated Ts7_86_Automation.java

--- src/test/java/com/vytrack/tests/Ts7_86_Automation.java
+++ src/test/java/com/vytrack/tests/Ts7_86_Automation.java
@@ -46,12 +46,8 @@ public class Ts7_86_Automation {
     WebElement pinIconText = driver.findElement(By.xpath("//*[@id=\"container\"]/div[2]/div/p[1]"));
     String actualText2 = pinIconText.getText();
     String expectedText2 = "Use the pin icon on the right top corner of the page to create fast access link in the pinbar.";
-   // Assert.assertEquals(actualText2, expectedText2);
-    if (actualText2.equals(expectedText2)){
-        System.out.println("Passed");
-    }else{
-        System.out.println("Failed");
-    }
+    Assert.assertEquals(actualText2, expectedText2);
+
 }
 
 
